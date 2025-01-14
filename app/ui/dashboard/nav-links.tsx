07ab37d6646c14260fe4e266fba7f4842e8bd356
Talha Khalid <talha.khan@grp.contegris.com>
2025-01-14 19:24:32 +0500

    chapter 7 changes

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -7,13 +7,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-
-
 import clsx from 'clsx';
 
-
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
